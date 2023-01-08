53dbbe0f7a61f5606db11b99bf892f101415d19c
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-08 16:36:10 +0700

    files upload
    model
    routes
    .gitignore

---
+++ backend/model/user.ts
@@ -0,0 +1,25 @@
+class Authenticator {
+    private studentIds: Set<string>;
+    private teacherIds: Set<string>;
+  
+    constructor(studentIds: string[], teacherIds: string[]) {
+      this.studentIds = new Set(studentIds);
+      this.teacherIds = new Set(teacherIds);
+    }
+  
+    public isStudent(id: string): boolean {
+      return this.studentIds.has(id);
+    }
+  
+    public isTeacher(id: string): boolean {
+      return this.teacherIds.has(id);
+    }
+  }
+  
+  const authenticator = new Authenticator(['123', '456', '789'], ['abc', 'def', 'ghi']);
+  
+  console.log(authenticator.isStudent('123')); // true
+  console.log(authenticator.isStudent('abc')); // false
+  console.log(authenticator.isTeacher('abc')); // true
+  console.log(authenticator.isTeacher('123')); // false
+  
