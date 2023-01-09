4a791f710850ed1c07b95568a8a39e281d4790af
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-10 01:31:59 +0700

    Fix : update mongoose services

--- backend/src/services/mongoose.service.ts
+++ backend/src/services/mongoose.service.ts
@@ -1,24 +1,42 @@
-import mongoose, { Collection } from "mongoose"
-import * as mongoDB from "mongodb"
-import * as dotenv from "dotenv"
+import * as argon2 from "argon2";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import { Request, Response } from "express";
 
-export const connectToDatabase = async () => {
+import { userSchema } from "../model/user";
+
+export const collections: { food?: mongoose.Collection, review?: mongoose.Collection } = {};
+
+export async function connectToDatabase() {
   dotenv.config();
+
   try {
-    await mongoose.connect(process.env.DB_CONN_STRING ||""), {
+    await mongoose.connect(process.env.DB_CONN_STRING || "", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-    };
-    console.log('MongoDB connected...');
+    });
+    
+
+    console.log(`Successfully connected to MongoDB at ${process.env.DB_CONN_STRING}`);
+
+    const db = mongoose.connection;
+
+    const foodCollection = db.collection("food");
+    collections.food = foodCollection;
+
+    const reviewCollection = db.collection("review");
+    collections.review = reviewCollection;
+
+    console.log(`Successfully accessed collections: ${foodCollection.collectionName}, ${reviewCollection.collectionName}`);
   } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err}`);
     process.exit(1);
   }
+}
 
-  const client: mongoose.Connect(process.env.DB_CONN_STRING ||"")
-  const db: mongoDB.Db = client.db(process.env.DB_NAME);
-
-
-
-  Collection.course = coursecollection;
-};
+async function hashPassword(pass: string): Promise<string> {
+  // Hash the password
+  const hash = await argon2.hash(pass);
+  return hash;
+}
