9e9c9eb046d0d6257e840bb73db94aa54730659f
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-09 17:05:54 +0700

    fix: login request and response

--- backend/src/loginsignup/login.post.ts
+++ backend/src/loginsignup/login.post.ts
@@ -1,27 +1,39 @@
 import * as argon2 from 'argon2';
 import * as uuid from 'uuid';
 import * as cookie from 'cookie';
+import { Request, Response } from 'express';
 
-async function login(username: string, password: string, res: Response): Promise<void> {
-  const user = await getUserByUsername(username);
+async function login(req: Request, res: Response): Promise<void> {
+  // Get email and password from the request body
+  const { email, password } = req.body;
+
+  // Look up the user by email
+  const user = await getUserByEmail(email);
   if (!user) {
-    throw new Error('Invalid username or password');
+    throw new Error('Invalid email or password');
   }
+
+  // Check if the provided password is correct
   const passwordHash = user.passwordHash;
   const correctPassword = await argon2.verify(passwordHash, password);
   if (!correctPassword) {
-    throw new Error('Invalid username or password');
+    throw new Error('Invalid email or password');
   }
+
+  // Generate a session code and set it in a cookie
   const sessionCode = uuid.v4();
   await setSessionCode(user.id, sessionCode);
   res.setHeader('Set-Cookie', cookie.serialize('session_code', sessionCode, {
     httpOnly: true,
     maxAge: 60 * 60 * 24 * 7 // 1 week
   }));
+
+  // Return the session code in the response
+  res.send({ sessionCode });
 }
 
-async function getUserByUsername(username: string): Promise<User | null> {
-  // Query database for user with the given username
+async function getUserByEmail(email: string): Promise<Email | null> {
+  // Query database for user with the given email
 }
 
 async function setSessionCode(userId: string, sessionCode: string): Promise<void> {
