fa2afc1b2cbc731c99eb27d91b5f966c2624f1a4
tingwry <weeraya972546@gmail.com>
2023-01-09 19:00:49 +0700

    fix: courses.ts

--- backend/src/model/courses.ts
+++ backend/src/model/courses.ts
@@ -1,5 +1,14 @@
-class Course {
-    title: string;
-    description: string;
-    teacherName: string;
-}
+import { model, Schema, Model, Document, InferSchemaType } from "mongoose";
+import mongoose from "mongoose";
+
+export type Course = InferSchemaType<typeof courseSchema>;
+
+export const courseSchema: Schema = new Schema(
+    {
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        instructorName: { type: String, required: true },
+    }
+)
+
+export const CourseModel = mongoose.model('Course', courseSchema);
