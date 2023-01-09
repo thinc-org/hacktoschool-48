21ba8782ecf4dd882ba05e77c41b37b83316ebc6
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-01-09 23:37:45 +0700

    Header Component

---
+++ frontend/components/header/header.js
@@ -0,0 +1,32 @@
+// import styles from '../styles/header.css'
+import React from 'react';
+import { Container } from 'reactstrap';
+import headerStyles from "../../styles/header.module.css";
+
+export default function header() {
+    return (
+        // <div>Header</div>
+        <div className={headerStyles.header}>
+            <Container>
+                <div className={headerStyles.content}>
+                    <div className={headerStyles.leftNav}>
+                        <div className={headerStyles.logo}><a href="#"><span className="DelaFont">GlobalTalk</span></a></div>
+                        <div className={headerStyles.manu}>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Textbook</a></li>
+                                <li><a href="#">Statistics</a></li>
+                                <li><a href="#">Games</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={headerStyles.login}>
+                        <div className={headerStyles.loginText}><a href="#">Login</a></div>
+                        <div className={headerStyles.signup}><a href="#">Sign up</a></div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    );
+}
