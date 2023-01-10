8c3f09d8b9c1d56ec2827e2a05f12c399f61f171
tingwry <weeraya972546@gmail.com>
2023-01-11 00:43:42 +0700

    fix: signup.post.ts

--- backend/src/loginsignup/signup.post.ts
+++ backend/src/loginsignup/signup.post.ts
@@ -1,8 +1,6 @@
 import * as argon2 from 'argon2';
 import { UserModel } from '../model/user';
 import { Request, Response } from 'express';
-import { model, Schema } from 'mongoose';
-import { collections } from '../services/mongoose.service';
 
 export async function hashPassword(pass: string): Promise<string> {
     // Hash the password
@@ -10,26 +8,37 @@ export async function hashPassword(pass: string): Promise<string> {
     return hash;
   }
 
-  // Get info from the request body
-  export async function signup(req: Request, res: Response): Promise<void> {
-      const { email, password, name, surname, id, courses } = req.body;
-      
-      collections?.user.findOne({email}).exec((err, user) => {
-          // Check if user already exists
-          if(user) {
-              return res.status(400).json({error: 'User already exists'});
-            }
-            
-        if (!collections) return res.status(500);
-        let newUser = new collections.user({ email, password: hashPassword(password), name, surname, id, courses });
-        newUser.save((err, success) => {
-            if(err) {
-                console.log(err);
-                return res.status(401).json({error: 'Error signing up, please try again.'})
-            }
-            return res.json({
-                message: 'Signup success! Please signin'
-            })
+// Get info from the request body
+export async function signup(req: Request, res: Response): Promise<void> {
+    const { email, password, name, surname, id, role } = req.body;
+    
+    // Validate input
+    if (!name || !email || !password || !role) {
+        return res.status(400).json({ message: "`email`, `password`, `name`, `surname`, `id` and `role` are required" });
+    }
+
+    // Check if user already exists
+    const user = await UserModel.findOne(({ email }));
+    if (user) {
+        return res.status(400).json({error: 'User already exists'});
+    }
+
+    // Create User
+    const newUser = new UserModel({ 
+        email, 
+        password: hashPassword(password), 
+        name, 
+        surname, 
+        id, 
+        role 
+    });
+    await newUser.save((err, success) => {
+        if(err) {
+            console.log(err);
+            return res.status(401).json({error: 'Error signing up, please try again.'})
+        }
+        return res.status(201).json({
+            message: 'Signup success! Please signin'
         })
-    })
-}
+    });
+};
