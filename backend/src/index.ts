e2779ef5bf0fb60732712f7999d42fa2a568c91d
tingwry <weeraya972546@gmail.com>
2023-01-11 00:48:10 +0700

    fix: index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 
 import { connectToDatabase } from "./services/mongoose.service"
-// import { router } from "./routes/hacktoschool.routes"
+import { router } from "./routes/hacktoschool.routes"
 import { userRouter } from "./routes/userloginsignup"
 
 async function main() {
@@ -19,6 +19,9 @@ async function main() {
 
     app.use(cors());
     app.use(express.json());
+
+    app.use(userRouter);
+    app.use(router);
 
     app.listen(port, () => {
             console.log(`Server started at http://localhost:${port}`);
