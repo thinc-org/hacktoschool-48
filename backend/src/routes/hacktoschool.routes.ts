1e2111226b6f50aa145cb502d20c4ba937390d79
Pataradanai Hansuksakul <116214675+Pataradanaimcky@users.noreply.github.com>
2023-01-10 12:00:57 +0700

    feat: add router

--- backend/src/routes/hacktoschool.routes.ts
+++ backend/src/routes/hacktoschool.routes.ts
@@ -4,6 +4,8 @@ const userRouter = require('./userloginsignup')
 import { Request,Response } from "express"
 import { collections } from '../services/mongoose.service';
 import { Course } from "../model/courses"
+import { MongooseError } from 'mongoose';
+import { getAllJSDocTagsOfKind } from 'typescript';
 
 export const router = Router();
 
@@ -11,10 +13,24 @@ const app = express();
 
 userRouter.post();
 
-router.get("/user/id", )
+router.get("/user/:id", async (req: Request, res: Response) => {
+    const id = req?.params?.id;
+
+    try{
+        const users = await collections?.user.find({});
+
+        res.status(200).send(users);
+    } catch (error) {
+        if (error instanceof MongooseError){
+            res.status(500).send(error.message);
+            return;
+        }     
+        else{
+            res.status(500).send("unknown error");
+            return;
+        }
+    }
+});
 
 router.get("/courses" , async (req:Request , res:Response) => {
-    try {
-        const Courses = (await collections?.course.findOne({}).toArray())
-    }
 });
