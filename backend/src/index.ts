fe43c185e707653d3ed93671d32aeef0af73b3a2
tingwry <weeraya972546@gmail.com>
2023-01-10 23:44:56 +0700

    fix: index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,42 @@
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import * as dotenv from "dotenv";
 
 import { connectToDatabase } from "./services/mongoose.service"
 // import { router } from "./routes/hacktoschool.routes"
 import { userRouter } from "./routes/userloginsignup"
 
-const app = express();
-const port = 3000;
+async function main() {
+    dotenv.config();
 
-app.use(cors())
-app.use(express.json())
+    // Connect mongoose to DB
+    await mongoose.connect(process.env.DB_CONN_STRING!)
 
-connectToDatabase()
+    // Create and start express app
+    const app = express();
+    const port = 3000;
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+    });
+}
+main();
+
+
+
+
+
+/* connectToDatabase()
     .then(() => {
         app.use("/", userRouter);
 
-        app.listen(port, () => {
-            console.log(`Server started at http://localhost:${port}`);
-        });
+        
     })
     .catch((error: Error) => {
         console.error("Database connection failed", error);
         process.exit();
-    });
+    }); */
