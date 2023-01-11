95e24a4cc733b1d2a6ee5b3a719537c9adbcea48
Fahnalan <nalanlakwong@gmail.com>
2023-01-11 20:06:52 +0700

    add click outside to close

--- frontend/components/header/header.js
+++ frontend/components/header/header.js
@@ -1,101 +1,136 @@
 // import styles from '../styles/header.css'
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { Container } from 'reactstrap';
+import React, { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+import { Container } from "reactstrap";
 import headerStyles from "../../styles/header.module.css";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 export default function header() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
 
-    const [isSignClicked, setSignClicked] = useState(false);
-    const signToggle = () => setSignClicked(!isSignClicked);
+  const [isSignClicked, setSignClicked] = useState(false);
+  const signToggle = () => setSignClicked(!isSignClicked);
 
-    const [click, setClick] = React.useState(false);
-    const handleClick = () => setClick(!click);
-    const setClickToFalse = () => setClick(false);
+  const [click, setClick] = React.useState(false);
+  const handleClick = () => setClick(!click);
+  const setClickToFalse = () => setClick(false);
 
-    return (
-        // <div>Header</div>
-        <div className={headerStyles.header}>
-            <Container>
-                <div className={headerStyles.content}>
-                    <div className={headerStyles.navIcon} onClick={handleClick}>
-                        <Link href={(!click) ? 'navigation' : '/'}>
-                            <i className="ri-menu-2-line"></i>
-                        </Link>
-                    </div>
+  let menuRef = useRef();
+  let signupRef = useRef();
+  // click outside to close Games
+  useEffect(() => {
+    let handler = (event) => {
+      if (!menuRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
 
-                    <div className={headerStyles.leftNav}>
-                        <div className={headerStyles.logo}><Link href="/"><span className="DelaFont" onClick={setClickToFalse}>GlobalTalk</span></Link></div>
-                        <div className={headerStyles.navbar}>
-                            <div className={headerStyles.manu}>
-                                <ul>
-                                    <li><Link href="/">
-                                        Home
-                                    </Link></li>
-                                    <li><Link href="/textbook">
-                                        Textbook
-                                    </Link></li>
-                                    <li><Link href="#">
-                                        Statistics
-                                    </Link></li>
-                                    <li className={headerStyles.dropdown}>
-                                        <Link href="#" onClick={toggle}>
-                                            <span>Games</span>
-                                            <i className="ri-arrow-down-s-line"></i>
-                                        </Link>
-                                        {
-                                            isOpen && (
-                                                <ul className={headerStyles.dropdownContent}>
-                                                    <li>
-                                                        <Link href="#">
-                                                            <span>Sprint</span>
-                                                            <i className="ri-arrow-right-line"></i>
-                                                        </Link>
-                                                    </li>
-                                                    <li>
-                                                        <Link href="#">
-                                                            <span>Audio-call</span>
-                                                            <i className="ri-arrow-right-line"></i>
-                                                        </Link>
-                                                    </li>
-                                                </ul>
-                                            )
-                                        }
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-                    <div className={headerStyles.login}>
-                        <div className={headerStyles.loginText}>
-                            <Link href="#" >
-                                <span>Log In</span>
-                                <i className="ri-arrow-right-line"></i>
-                            </Link>
-                        </div>
+  // click outside to close Sign Up
+  useEffect(() => {
+    let signuphandler = (event) => {
+      if (!signupRef.current.contains(event.target)) {
+        setSignClicked(false);
+      }
+    };
+    document.addEventListener("mousedown", signuphandler);
 
-                        <div>
-                            <div className={headerStyles.signupbtn} onClick={signToggle}>Sign up</div>
-                            {/* {isSignClicked ? <p>True</p> : <p>False</p>} */}
-                            {isSignClicked && (
-                                <div className={headerStyles.dropSignInContent}>
-                                    <Link href='#'>
-                                        <div>For student</div>
-                                    </Link >
-                                    <Link href='#'>
-                                        <div>For instructor</div>
-                                    </Link>
-                                </div>
-                            )
-                            }
-                        </div>
-                    </div>
+    return () => {
+      document.removeEventListener("mousedown", signuphandler);
+    };
+  });
+
+  return (
+    // <div>Header</div>
+    <div className={headerStyles.header}>
+      <Container>
+        <div className={headerStyles.content}>
+          <div className={headerStyles.navIcon} onClick={handleClick}>
+            <Link href={!click ? "navigation" : "/"}>
+              <i className="ri-menu-2-line"></i>
+            </Link>
+          </div>
+
+          <div className={headerStyles.leftNav}>
+            <div className={headerStyles.logo}>
+              <Link href="/">
+                <span className="DelaFont" onClick={setClickToFalse}>
+                  GlobalTalk
+                </span>
+              </Link>
+            </div>
+            <div className={headerStyles.navbar}>
+              <div className={headerStyles.manu}>
+                <ul>
+                  <li>
+                    <Link href="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link href="/textbook">Textbook</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Statistics</Link>
+                  </li>
+                  <li ref={menuRef} className={headerStyles.dropdown}>
+                    <Link href="#" onClick={toggle}>
+                      <span>Games</span>
+                      <i className="ri-arrow-down-s-line"></i>
+                    </Link>
+                    {isOpen && (
+                      <ul className={headerStyles.dropdownContent}>
+                        <li>
+                          <Link href="#">
+                            <span>Sprint</span>
+                            <i className="ri-arrow-right-line"></i>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="#">
+                            <span>Audio-call</span>
+                            <i className="ri-arrow-right-line"></i>
+                          </Link>
+                        </li>
+                      </ul>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className={headerStyles.login}>
+            <div className={headerStyles.loginText}>
+              <Link href="#">
+                <span>Log In</span>
+                <i className="ri-arrow-right-line"></i>
+              </Link>
+            </div>
+
+            <div ref={signupRef}>
+              <div className={headerStyles.signupbtn} onClick={signToggle}>
+                Sign up
+              </div>
+              {/* {isSignClicked ? <p>True</p> : <p>False</p>} */}
+              {isSignClicked && (
+                <div className={headerStyles.dropSignInContent}>
+                  <Link href="#">
+                    <div>For student</div>
+                  </Link>
+                  <Link href="#">
+                    <div>For instructor</div>
+                  </Link>
                 </div>
-            </Container>
+              )}
+            </div>
+          </div>
         </div>
-    );
+      </Container>
+    </div>
+  );
 }
