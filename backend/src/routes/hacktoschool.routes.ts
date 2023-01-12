6549a705a52923bac60272f4a4c75ce835f0b17a
tingwry <weeraya972546@gmail.com>
2023-01-12 17:06:48 +0700

    fix: hacktoschool.routes.ts (add endpoints)

--- backend/src/routes/hacktoschool.routes.ts
+++ backend/src/routes/hacktoschool.routes.ts
@@ -1,17 +1,13 @@
 import { Router } from 'express';
-const express = require('express')
-const userRouter = require('./userloginsignup')
-import { Request,Response } from "express"
+const express = require('express');
+import { Request,Response } from "express";
 import { MongooseError } from 'mongoose';
-import { getAllJSDocTagsOfKind } from 'typescript';
 import { UserModel } from '../model/user';
 import { CourseModel } from '../model/courses';
 import { TokenPayload } from '../loginsignup/login.post'
 import * as jwt from "jsonwebtoken";
 
 export const router = Router();
-
-const app = express();
 
 // get 1 user
 router.get("/user/:id", async (req: Request, res: Response) => {
@@ -48,7 +44,7 @@ router.get("/user/:id", async (req: Request, res: Response) => {
     }
 });
 
-// get all courses
+// get all courses (show all except students enrolled)
 router.get("/course", async (req:Request , res:Response) => {
     
     // Check if token exists
@@ -64,7 +60,7 @@ router.get("/course", async (req:Request , res:Response) => {
     } catch {
         return res.status(401).json({ message: "Invalid token" });
     }
-    function exceptStudent() {
+    /* function exceptStudent() {
 
         const nonstudent = {
             student: false
@@ -72,18 +68,69 @@ router.get("/course", async (req:Request , res:Response) => {
         CourseModel.find({} ,nonstudent, function (err,nonStudent){
             return nonStudent
         });
-    }
-    const courses = await CourseModel.find();
-    // Check if user is instructor
-    // If not instructor, can view all properties except students enrolled
-    if (user.role !== "instructor") {
-        return exceptStudent(courses);
-    }
-    // Instructors can view students enrolled
+    } */
+    const courses = await CourseModel.find().select("title description instructorName level");
     return courses;
 });
 
 //lines 70 & 73 undone maiwai laew
+
+
+// instructor get all their courses
+router.get("/course/mycourses", async (req: Request, res: Response) => {
+    // Check if token exists
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "No token found in Authorization header" });
+    }
+
+    // Validate token
+    let user: TokenPayload;
+    try {
+        user = jwt.verify(token, process.env.JWT_SECRET!) as TokenPayload;
+    } catch {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+    // Check if the user is instructor
+    const User = await UserModel.findOne({ id: user.id });
+    if (User?.role !== 'instructor') {
+        return res.status(403).json({ message: "Only instructors can view their courses" });
+    }
+    
+    const userCourses = await CourseModel.find({ instructorName: User.name });
+    return userCourses;
+});
+
+
+// instructor get students enrolled for 1 course
+router.get("/course/mycourses/:title", async (req: Request, res: Response) => {
+    // Check if token exists
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).json({ message: "No token found in Authorization header" });
+    }
+
+    // Validate token
+    let user: TokenPayload;
+    try {
+        user = jwt.verify(token, process.env.JWT_SECRET!) as TokenPayload;
+    } catch {
+        return res.status(401).json({ message: "Invalid token" });
+    }
+
+
+    // Check if the user is instructor
+    const User = await UserModel.findOne({ id: user.id });
+    if (User?.role !== 'instructor') {
+        return res.status(403).json({ message: "Only instructors can view enrolled students" });
+    }
+
+    const title = req?.params?.title;
+    const course = await CourseModel.findOne({ instructorName: User.name, title });
+    return course?.student;
+})
+
 
 
 // student enroll in a course
@@ -106,8 +153,9 @@ router.post("/course/:title", async (req: Request, res: Response) => {
     const title = req?.params?.title;
 
     try{
+        const User = await UserModel.findOne({ id: user.id });
         const course = await CourseModel.findOne({ title });
-        course?.student.push(user);
+        course?.student.push(User);
         
     } catch (error) {
         if (error instanceof MongooseError){
@@ -139,7 +187,7 @@ router.post("/course", async (req: Request, res: Response) => {
     }
 
     // Check if user is instructor
-    if (user.role !== "instrctor") {
+    if (user.role !== "instructor") {
         return res.status(403).json({ message: "Only instructors can create course" });
     }
 
