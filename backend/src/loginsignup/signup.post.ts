4c18d095a724d1bc9b000f588a01ce9bc026e30f
tingwry <weeraya972546@gmail.com>
2023-01-12 18:19:08 +0700

    fix: add signup functions and endpoints for student and instructor

--- backend/src/loginsignup/signup.post.ts
+++ backend/src/loginsignup/signup.post.ts
@@ -9,12 +9,12 @@ export async function hashPassword(pass: string){
   }
 
 // Get info from the request body
-export async function signup(req: Request, res: Response){
-    const { email, password, name, surname, role } = req.body;
+export async function signupStudent(req: Request, res: Response){
+    const { email, password, name, surname } = req.body;
     
     // Validate input
-    if (!email || !password || !name || !surname || !role) {
-        res.status(400).json({ message: "`email`, `password`, `name`, `surname`, and `role` are required" }).send();
+    if (!email || !password || !name || !surname) {
+        res.status(400).json({ message: "`email`, `password`, `name`, and `surname` are required" }).send();
     }
 
     // Check if user already exists
@@ -29,7 +29,41 @@ export async function signup(req: Request, res: Response){
         passwordHash: await hashPassword(password), 
         name, 
         surname, 
-        role 
+        role: "student"
+    });
+    newUser.save((err, success) => {
+        if(err) {
+            console.log(err);
+            return res.status(401).json({error: 'Error signing up, please try again.'})
+        }
+        return res.status(201).json({
+            message: 'Signup success! Please signin'
+        })
+    });
+};
+
+
+export async function signupInstructor(req: Request, res: Response){
+    const { email, password, name, surname } = req.body;
+    
+    // Validate input
+    if (!email || !password || !name || !surname) {
+        res.status(400).json({ message: "`email`, `password`, `name`, and `surname` are required" }).send();
+    }
+
+    // Check if user already exists
+    const user = await UserModel.findOne(({ email }));
+    if (user) {
+        res.status(400).json({error: 'User already exists'}).send();
+    }
+
+    // Create User
+    const newUser = new UserModel({ 
+        email, 
+        passwordHash: await hashPassword(password), 
+        name, 
+        surname, 
+        role: "instructor"
     });
     newUser.save((err, success) => {
         if(err) {
--- backend/src/routes/userloginsignup.ts
+++ backend/src/routes/userloginsignup.ts
@@ -1,11 +1,12 @@
 //import dependencies
-const express = require('express')
-import {login} from '../loginsignup/login.post'
-import {signup} from '../loginsignup/signup.post'
+const express = require('express');
+import {login} from '../loginsignup/login.post';
+import { signupInstructor, signupStudent } from '../loginsignup/signup.post';
 
 export const userRouter = express.Router();
 
 //using login function need to be use /login after api first
-userRouter.post("/login", login)
+userRouter.post("/login", login);
 //using signup function need to be use /signup after api first
-userRouter.post("/signup", signup)
+userRouter.post("/signup/student", signupStudent);
+userRouter.post("/signup/instructor", signupInstructor);
