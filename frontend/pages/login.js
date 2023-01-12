e0e3528c779943a6e17a822b47d08c2fa61e5d5f
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-01-13 02:43:32 +0700

    Login Page

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,10 +2,52 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import PageStyles from '../styles/std_signup.module.css';
-
 import loginImage from '../asset/login/loginpage.jpeg';
 
 export default function login_signup() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+
+        const jsonData = {
+            email: data.get('email'),
+            password: data.get('password'),
+        }
+        // const API_URL = "https://sour-times-scream-49-230-141-85.loca.lt/login"
+        const API_URL = "http://localhost:4000/login"
+
+        fetch(API_URL, {
+            method: 'POST',
+            crossDomain: true,
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify(jsonData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data,"userLogin");
+                if (data.token) {
+                    if (jsonData.email !== "admin") {
+                        window.localStorage.setItem("token", data.token)
+                        alert("Login success!")
+                    } else {
+                        window.localStorage.setItem("token", data.token)
+                        alert("Login success!")
+                    }
+                    window.location.pathname = "/";
+                } else {
+                    alert("login failed");
+                }
+            })
+            .catch((error) => {
+                alert("Login failed! Please sign up before logging in.");
+                console.error('Error:', error);
+            });
+    };
+
     return (
         <div className={PageStyles.stdPage}>
             <Image
@@ -17,7 +59,7 @@ export default function login_signup() {
             <div className={PageStyles.loginForm}>
                 <div className={PageStyles.fStd}>Welcome</div>
                 <div className={PageStyles.signupText}>LOG IN</div>
-                <form className={PageStyles.form}>
+                <form className={PageStyles.form} onSubmit={handleSubmit}>
                     <label>
                         <p>Email</p>
                         <input type="text" name="email" />
