b9645ac58aa83921b4e2f892a77f1ea987a7b4af
Fahnalan <nalanlakwong@gmail.com>
2023-01-13 22:11:03 +0700

    Update user_details.js

--- frontend/pages/user_details.js
+++ frontend/pages/user_details.js
@@ -1,45 +1,50 @@
 import React, { Component } from "react";
 
 export default class UserDetail extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            userData: "",
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: "",
+    };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    fetch("http://localhost:4000/user", {
+      method: "POST",
+      crossDomail: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authorization: token,
+      },
+      body: JSON.stringify({
+        token: window.localStorage.getItem("token"),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data, "userdata");
+        this.setState({ userData: data.data });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-        fetch('http://localhost:4000/user', {
-            method: 'POST',
-            crossDomail: true,
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: "application/json",
-                "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify({
-                token: window.localStorage.getItem("token"),
-            }),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data, "userdata");
-                this.setState({ userData: data.data })
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
-
-    render() {
-        return (
-            <div className="user-detail">
-                <div><h1>User : {this.state.userData.name} {this.state.userData.surname}</h1></div>
-                <div><h1>Email : {this.state.userData.email}</h1></div>
-            </div>
-                
-
-        );
-    }
+  render() {
+    return (
+      <div className="user-detail">
+        <div>
+          <h1>
+            User : {this.state.userData.name} {this.state.userData.surname}
+          </h1>
+        </div>
+        <div>
+          <h1>Email : {this.state.userData.email}</h1>
+        </div>
+      </div>
+    );
+  }
 }
