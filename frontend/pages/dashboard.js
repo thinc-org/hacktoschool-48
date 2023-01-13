cf805ce107c72f164810e2c557cd4a9fd55229b9
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-01-13 17:27:10 +0700

    Dashboard Page

---
+++ frontend/pages/dashboard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from '../components/product_card/card';
+import img1 from '../asset/courses/courseimg1.jpg';
+
+import dashboardStyles from '../styles/dashboard.module.css';
+
+const data = [
+    // Use alt+z to wrap the code (window), comand+z (macOs)
+    {
+        image: img1,
+        courseName: 'Introduction to Programming',
+        level: 'Easy',
+        instructor: 'Mr. James Smith',
+        detail: 'Learn HTML, CSS, Python, and JavaScript. Get projects that demonstrate your grasp of coding fundamentals, and build confidence in your ability to think and problem-solve like a programmer.',
+    },
+]
+
+export default function dashboard() {
+    return (
+        <div className={dashboardStyles.Card}>
+            <div className={dashboardStyles.topSection}>
+                <span className={dashboardStyles.yourCourse}>Your Courses</span>
+                <div className={dashboardStyles.rightSide}>
+                    <i class="ri-add-circle-fill"></i>
+                    <span className={dashboardStyles.createCourse}>CREAT COURSE</span>
+                </div>
+                
+            </div>
+            <div>
+                <Card item={data[0]}/>
+            </div>
+        </div>
+    );
+}
